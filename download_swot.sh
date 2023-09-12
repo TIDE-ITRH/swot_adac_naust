22e7bc70f3e43cb5faa6f0ed69a21e79705f4121
mrayson <matt.rayson@gmail.com>
2023-09-12 06:21:59 +0000

    New stuff...

--- download_swot.sh
+++ download_swot.sh
@@ -1,4 +1,6 @@
 # Setup an rclone folder for "ftp-access.aviso.altimetry.fr" first
-rclone copy --progress swot:/data/Data/ALTI/DUACS_SWOT_Nadir/L3_Along_track ~/data/SWOT/
-rclone copy --progress swot:/data/Cruises/NWshelf/Bulletin REPORTS/
+#rclone copy --progress swot:/data/Data/ALTI/DUACS_SWOT_Nadir/L3_Along_track ~/data/SWOT/
+#rclone copy --progress swot:/data/Cruises/NWshelf/Bulletin REPORTS/
  
+#rclone copy -P --transfers=4 aviso_ftp:/data/swot_beta_products/karin/l2_lr_ssh/basic/ ./
+#rclone copy -P --transfers=4 aviso_ftp:/data/swot_beta_products/karin/l2_lr_ssh/UnSmoothed/ ./
