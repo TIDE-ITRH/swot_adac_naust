91147eebe9103f904af345683e9b453cc7c24246
mrayson <matt.rayson@gmail.com>
2023-10-19 01:18:14 +0000

    updated karin and ghrsst scripts

---
+++ make_gif.sh
@@ -0,0 +1,5 @@
+#convert -delay 100 -loop 0 GHRSST*.png myimage.gif
+convert -delay 100 -loop 0 FIGURES/*nicemap_v2*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_v2.gif
+convert -delay 100 -loop 0 FIGURES/*nicemap_grad*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_grad.gif
+
+
