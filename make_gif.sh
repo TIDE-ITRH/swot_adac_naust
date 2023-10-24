6b59a8198886dcdc74eb88cb632270b7031839fe
mrayson <matt.rayson@gmail.com>
2023-10-24 02:20:11 +0000

    Calculated tidal harmonics for the pilbara SWOT region

--- make_gif.sh
+++ make_gif.sh
@@ -1,5 +1,7 @@
 #convert -delay 100 -loop 0 GHRSST*.png myimage.gif
-convert -delay 100 -loop 0 FIGURES/*nicemap_v2*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_v2.gif
-convert -delay 100 -loop 0 FIGURES/*nicemap_grad*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_grad.gif
+convert -delay 100 -loop 0 FIGURES/SWOT/*polycorr*2023-06*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_v3.gif
+
+#convert -delay 100 -loop 0 FIGURES/*nicemap_v2*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_v2.gif
+#convert -delay 100 -loop 0 FIGURES/*nicemap_grad*.png FIGURES/SWOT/SWOT_Karin_corrected_nicemap_grad.gif
 
 
