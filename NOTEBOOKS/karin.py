8109f6d8ad444ec52c39efadc261e87199d8981c
mrayson <matt.rayson@gmail.com>
2024-05-26 23:12:55 +0000

    SWOT L2 L3 processing

--- NOTEBOOKS/karin.py
+++ NOTEBOOKS/karin.py
@@ -2,8 +2,135 @@
 SWOT Karin utils
 """
 import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 
-def plot_karin(ds_area, ii,var1= 'ssha_karin', var2='ssha_karin_2', outfile=None):
+def clip_swot(ds, area):
+    selection = (
+        (ds.longitude > area[0]) &
+        (ds.longitude < area[2]) &
+        (ds.latitude > area[1]) &
+        (ds.latitude < area[3]))
+    selection = selection.compute()
+    return ds.where(selection, drop=True)
+
+# Remove flagged data
+def apply_flag(dataflag, da, flags='good'):
+    if flags == 'good':
+        upperflag = 0
+    elif flags == 'suspect':
+        upperflag = 8192
+    elif flags == 'degraded':
+        upperflag = 262144
+    elif flags == 'bad':
+        upperflag = np.inf
+
+    idx = dataflag <= upperflag
+
+    return da.where(idx, np.nan)
+
+def calc_ssha_l2_l3_dynamic(ds_l2, ds_l3, flags='good'):
+    ssha = ds_l2['ssh_karin_2']\
+        + ds_l2['height_cor_xover']\
+        - ds_l3['mss'] \
+        - ds_l2['solid_earth_tide']\
+        - ds_l3['ocean_tide']\
+        - ds_l2['pole_tide']\
+        - ds_l2['dac']
+
+    return apply_flag( ds_l2['ssh_karin_2_qual'], ssha, flags=flags)
+
+def calc_ssha_l2_l3_all(ds_l2, ds_l3, flags='good'):
+    ssha = ds_l2['ssh_karin_2']\
+        + ds_l3['calibration']\
+        - ds_l3['mss'] \
+        - ds_l2['solid_earth_tide']\
+        - ds_l3['ocean_tide']\
+        - ds_l2['pole_tide']\
+        - ds_l2['dac']
+
+    return apply_flag( ds_l2['ssh_karin_2_qual'], ssha, flags=flags)
+
+def process_l2_l3(l2file, l3file, flags, area=None):
+    ds_l2_ = xr.open_dataset(l2file)
+    ds_l3_ = xr.open_dataset(l3file)
+
+    if area is None:
+        ds_l2 = ds_l2_
+        ds_l3 = ds_l3_
+    else:
+        ds_l2 = clip_swot(ds_l2_, area)
+        ds_l3 = clip_swot(ds_l3_, area)
+
+    ssha_l3 = ds_l3['ssha']
+    ssha_l2 = apply_flag(ds_l2['ssha_karin_2_qual'],
+        ds_l2['ssha_karin_2'] + ds_l2['height_cor_xover'], flags=flags)
+    
+    ssha_l2_l3_all = calc_ssha_l2_l3_all(ds_l2, ds_l3, flags=flags)
+    ssha_l2_l3_dynamic = calc_ssha_l2_l3_dynamic(ds_l2, ds_l3, flags=flags)
+    
+    # Create a new dataset
+    ds = xr.Dataset(
+        {'ssha_l3':ssha_l3,
+         'ssha_l2':ssha_l2,
+         'ssha_l2_l3_all':ssha_l2_l3_all,
+         'ssha_l2_l3_dynamic':ssha_l2_l3_dynamic,
+         'time':ds_l2['time'],
+        })
+    
+    ds['ssha_l2'].attrs = ds['ssha_l3'].attrs
+    ds['ssha_l2'].attrs['comment'] = \
+        "ds_l2['ssha_karin_2'] + ds_l2['height_cor_xover']"
+    ds['ssha_l2_l3_all'].attrs['comment'] = \
+        "ssha = ds_l2['ssh_karin_2']\
+            + ds_l3['calibration']\
+            - ds_l3['mss'] \
+            - ds_l2['solid_earth_tide']\
+            - ds_l3['ocean_tide']\
+            - ds_l2['pole_tide']\
+            - ds_l2['dac']\
+        "
+    ds['ssha_l2_l3_dynamic'].attrs['comment'] = \
+        "ssha = ds_l2['ssh_karin_2']\
+            + ds_l2['height_cor_xover']\
+            - ds_l3['mss'] \
+            - ds_l2['solid_earth_tide']\
+            - ds_l3['ocean_tide']\
+            - ds_l2['pole_tide']\
+            - ds_l2['dac']\
+        "
+    ds.attrs = dict(
+        l2_version = ds_l2.attrs['references'],
+        l3_version = ds_l3.attrs['product_version'],
+        l2_file = l2file,
+        l3_file = l3file,
+        quality_flag = flags,
+    )
+
+    return ds
+    
+def plot_karin(da, 
+            cmap="RdBu_r",
+            vmin=-0.3,
+            vmax=0.3,
+            fig=None,
+            ax=None):
+
+    if fig is None:
+        fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+    plot_kwargs = dict(
+    x="longitude",
+    y="latitude",
+    cmap=cmap,
+    vmin=vmin,
+    vmax=vmax,
+    cbar_kwargs={"shrink": 0.3},)
+    da.plot.pcolormesh(ax=ax, **plot_kwargs)
+    ax.set_aspect('equal')
+    #ax1.set_title(ds_area.satpass.values[ii])
+    #plt.tight_layout()
+
+def plot_karin_old(ds_area, ii,var1= 'ssha_karin', var2='ssha_karin_2', outfile=None):
         
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 4))
     plot_kwargs = dict(
--- download_swot.sh
+++ download_swot.sh
@@ -8,8 +8,33 @@
 #rclone lsd aviso_ftp:/data/swot_beta_products/l3_karin_nadir/1day_orbit/expert/alpha_v0_2
 ###
 #remote=aviso_ftp:/data/swot_beta_products/l3_karin_nadir/1day_orbit/expert/alpha_v0_3
-remote=aviso_ftp:/data/l3_karin_nadir/1day_orbit/expert/alpha_v0_3
-local=~/data/SWOT/l3_karin_nadir_v0_3
-#include="*_021_*.nc"
-include="*_008_*.nc"
+#remote=aviso_ftp:/data/l3_karin_nadir/1day_orbit/expert/alpha_v0_3
+#local=~/data/SWOT/l3_karin_nadir_v0_3
+##include="*_021_*.nc"
+#include="*_008_*.nc"
+#rclone copy -P --transfers 4 $remote --include $include $local
+
+#remote=aviso_ftp:/data/l3_karin_nadir/1day_orbit/expert/alpha_v0_3
+#remote=aviso:/swot_products/l3_karin_nadir/l3_lr_ssh/v1_0/Expert/
+#local=~/data/SWOT/l3_karin_nadir_v1_0
+#include="cycle_0*/*_062_*.nc" # Ningaloo
+#include="cycle_0*/*_131_*.nc" # Ningaloo
+#include="cycle_5*/*_021_*.nc" # Browse
+#include="cycle_4*/*_021_*.nc" # Browse
+#include="cycle_4*/*_008_*.nc" # Pilbara
+#include="cycle_5*/*_008_*.nc" # Pilbara
+#rclone copy -P --transfers 4 $remote --include $include $local
+
+
+#remote=aviso:/swot_products/l2_karin/l2_lr_ssh/PGC0/Unsmoothed/
+#local=~/data/SWOT/l2_karin_unsmoothed
+#include="cycle_0*/*_062_*.nc" # Exmouth (cross-shore)
+#include="cycle_0*/*_131_*.nc" # Exmouth (along-shore)
+#rclone copy -P --transfers 4 $remote --include $include $local
+
+remote=aviso:/swot_products/l2_karin/l2_lr_ssh/PGC0/Expert/
+local=~/data/SWOT/l2_karin_v1_0
+include="cycle_[4-5]*/*_021_*.nc" # Browse
 rclone copy -P --transfers 4 $remote --include $include $local
+
+
