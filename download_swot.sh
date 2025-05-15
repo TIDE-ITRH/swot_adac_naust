d0697aec5f3b199777fc546767dbaab7247f05d6
mrayson <matt.rayson@gmail.com>
2025-05-15 05:05:12 +0000

    updated SWOT processing

--- download_swot.sh
+++ download_swot.sh
@@ -28,17 +28,17 @@
 #####
 # Unsmoothed data
 #####
-remote=aviso:/swot_products/l3_karin_nadir/l3_lr_ssh/v2_0_1/Unsmoothed
-local=$MYGROUP/data/SWOT/l3_karin_unsmoothed_v2_0_1
+#remote=aviso:/swot_products/l3_karin_nadir/l3_lr_ssh/v2_0_1/Unsmoothed
+#local=$MYGROUP/data/SWOT/l3_karin_unsmoothed_v2_0_1
 #remote=aviso:/swot_products/l2_karin/l2_lr_ssh/PGC0/Unsmoothed/
 #local=~/data/SWOT/l2_karin_unsmoothed
 #remote=aviso:/swot_products/l3_karin_nadir/l3_lr_ssh/v1_0_2/Unsmoothed/
 #local=~/data/SWOT/l3_karin_unsmoothed
-include="cycle_50*/*_021_*.nc" # Browse (test)
+#include="cycle_50*/*_021_*.nc" # Browse (test)
 #include="cycle_[4-5]*/*_021_*.nc" # Browse
 ##include="cycle_0*/*_062_*.nc" # Exmouth (cross-shore)
 ##include="cycle_0*/*_131_*.nc" # Exmouth (along-shore)
-rclone copy -P --checkers 1 --transfers 2 $remote --include $include $local
+#rclone copy -P --checkers 1 --transfers 2 $remote --include $include $local
 #rclone copy -P $remote --include $include $local
 
 #remote=aviso:/swot_products/l2_karin/l2_lr_ssh/PGC0/Expert/
@@ -49,26 +49,28 @@ rclone copy -P --checkers 1 --transfers 2 $remote --include $include $local
 ###
 # Science
 ###
-#remote=aviso:/swot_products/l3_karin_nadir/l3_lr_ssh/v2_0/Expert/
-#local=~/data/SWOT/l3_karin_nadir_v2_0
+remote=aviso:/swot_products/l3_karin_nadir/l3_lr_ssh/v2_0_1/Expert/
+local=~/data/SWOT/l3_karin_nadir_v2_0_1
 ## Pilbara
-##strings='021 103 034 312'
-##strings='381 409'
-##strings='340'
+#strings='021 103 034 312 381 409 340'
 #
 ## Browse Science
-##strings='353 256 047 534 325 228'
+strings='353 256 047 534 325 228'
 #
 ## Lombok
 #strings='437 131 006'
+
+## Exmouth
+#strings='187 465 159 437 131 409 103 118 396 090 368 062 340 034'
 #
 ## Loop through the list
-#for string in $strings; do
-#  echo "$string"
-#  include="cycle_0*/*Expert_*_$string*.nc" # Browse
-#  echo $include
-#  rclone copy -P --transfers 4 $remote --include $include $local
-#done
+for string in $strings; do
+  echo "$string"
+  include="cycle_0*/*Expert_*_$string*.nc" # Browse
+  echo $include
+  #rclone copy -P --transfers 4 $remote --include $include $local
+  rclone copy -P --checkers 1 --transfers 2 $remote --include $include $local
+done
 #
 
 
