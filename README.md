7c03cda10d6dc7ce8c3bcfb3187ffd04de104e72
mrayson <matt.rayson@gmail.com>
2026-06-19 07:39:30 +0000

    Supp material figure generation

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Code for real-time viewing for the Northern Australia SWOT Adopt-a-crossover
 
 ## GRL submission code
+
+### Sea level processing
 - Sea level calculations: [steric height](NOTEBOOKS/calc_steric_height_moorings.ipynb), [hydrostatic sea level](NOTEBOOKS/calc_sealevel_mooring.ipynb)
+
+### Figures
 - [Figures 1,2](NOTEBOOKS/plot_swot_and_mooring_grl.ipynb)
 - [Figures 3,4](NOTEBOOKS/compare_swot_mooring_ssh_grl.ipynb)
 
---
+++ download_swot_grl.sh
@@ -0,0 +1,21 @@
+#####
+# Unsmoothed data
+#####
+MYGROUP=~/
+
+local=/data/SWOT_Cruise/GRL_archive/SWOT/
+
+PASS=021
+
+for CYCLE in 500 501 502 504 511 513 514 515 521 523 524 532 533;do
+    include="cycle_"$CYCLE"/*_"$PASS"_*.nc"
+    echo $include
+
+    # Unsmoothed
+    remote=aviso:/swot_products/l2_karin/l2_lr_ssh/PGC0/Unsmoothed/
+    rclone copy -P --transfers 1 $remote --include $include $local
+    #
+    # Smoothed
+    remote=aviso:/swot_products/l2_karin/l2_lr_ssh/PGC0/Expert/
+    rclone copy -P --checkers 1 --transfers 1 $remote --include $include $local
+done
